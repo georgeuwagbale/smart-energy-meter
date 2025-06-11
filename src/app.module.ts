220fe639c1ea73126cd55d0b8f68a363511368a6
georgeuwagbale <uwagbalegeorge@gmail.com>
2025-06-11 21:33:37 +0100

    refactor: simplify TypeOrmModule configuration and make ConfigModule global

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,29 +4,23 @@ import { AppService } from './app.service';
 import { EnergyModule } from './modules/energy/energy.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnergyReading } from './modules/energy/entities/energy-reading.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     EnergyModule,
-    ConfigModule.forRoot({
-      isGlobal: true, // makes config accessible throughout the app
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST', 'localhost'),
-        username: configService.get('DB_USERNAME', 'my_user'),
-        password: configService.get('DB_PASSWORD', 'my_password'),
-        database: configService.get('DB_NAME', 'energy'),
-        entities: [EnergyReading],
-        synchronize: configService.get('DB_SYNCHRONIZE', true),
-        extra: {
-          charset: 'utf8mb4_unicode_ci',
-        },
-      }),
-      inject: [ConfigService],
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [EnergyReading],
+      synchronize: true,
+      extra: {
+        charset: 'utf8mb4_unicode_ci',
+      },
     }),
   ],
   controllers: [AppController],
