d697d2551fb4b7d72d7fdbd6e39eea34b94137c5
georgeuwagbale <uwagbalegeorge@gmail.com>
2025-06-11 20:43:31 +0100

    feat: add @nestjs/config for environment configuration and set up Docker and Docker Compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,29 @@ import { AppService } from './app.service';
 import { EnergyModule } from './modules/energy/energy.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnergyReading } from './modules/energy/entities/energy-reading.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     EnergyModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      username: 'my_user',
-      password: 'my_password',
-      database: 'energy',
-      // entities: [__dirname + '/../**/*.entity.{js,ts}'],
-      entities: [EnergyReading],
-      synchronize: true,
-      extra: {
-        charset: 'utf8mb4_unicode_ci',
-      },
+    ConfigModule.forRoot({
+      isGlobal: true, // makes config accessible throughout the app
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST', 'localhost'),
+        username: configService.get('DB_USERNAME', 'my_user'),
+        password: configService.get('DB_PASSWORD', 'my_password'),
+        database: configService.get('DB_NAME', 'energy'),
+        entities: [EnergyReading],
+        synchronize: configService.get('DB_SYNCHRONIZE', true),
+        extra: {
+          charset: 'utf8mb4_unicode_ci',
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
